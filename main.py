96ab6832b94267b38e43162339737451891a27d3
Demonium <demoniumxxx@gmail.com>
2024-02-03 12:18:58 +0700

    Делаю ДЗ

---
+++ main.py
@@ -0,0 +1,12 @@
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://52c5fc5181ec754a8cbb018e8d5e0087@us.sentry.io/4506680887345152",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=1.0,
+)
